9e75779596622b1bf98ab8e3db7a4b484eb5002c
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2023-03-23 15:37:10 -0700

    Completed CustomerHttpReponse and passed test in CustomerHttpReponseTest

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,72 +1,30 @@
 import request.ParsedRequest;
+import com.google.gson.Gson;
+import response.CustomHttpResponse;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Tester {
 
     public static void main(String[] args) {
+        String status = "200 Success";
+        String version = "HTTP/1.1";
+        String body = "Response toString test";
+        Map<String,String> headers = new HashMap<>();
 
-        String test1 = "GET /hello HTTP/1.1\n"
-                + "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n"
-                + "Accept-Language: en-us\n"
-                + "Accept-Encoding: gzip, deflate\n"
-                + "Connection: Keep-Alive";
+        headers.put("Server","Apache");
+        headers.put("Content-Encoding","gzip");
+        headers.put("Content-Type","text/html; charset=utf-8");
 
-        String test2 = "STUFF /hello?location=sfsu&state=california HTTP/1.1\n"
-                + "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n"
-                + "Accept-Language: en-us\n"
-                + "Accept-Encoding: gzip, deflate\n"
-                + "Connection: Keep-Alive";
-        parse(test1);
-//        parse(test2);
+        CustomHttpResponse response = new CustomHttpResponse(headers, status, version, body);
 
+        System.out.println(response.toString());
 
     }
 
-    static void parse(String input){
 
-        ParsedRequest request = new ParsedRequest();
-        String[] HTTPmessages = input.split("\n");
 
-        //method parse
-        String[] firstTextLine = HTTPmessages[0].split(" ");
-        request.setMethod(firstTextLine[0]);
-
-        //path parse
-        String path = firstTextLine[1];
-        if (!path.contains("?")){
-            request.setPath(path);
-            path = "";
-        }
-        else{
-            request.setPath(path.substring(0,path.indexOf("?")));
-            path = path.substring(path.indexOf("?")+1);
-        }
-
-        //query parse
-        while(path.length()!=0){
-            if (path.contains("&")){//more than 1 query parameter
-                request.setQueryParam(path.substring(0,path.indexOf("=")), path.substring( (path.indexOf("=")+1),path.indexOf("&")));
-                path = path.substring(path.indexOf("&")+1);
-                continue;
-            }
-            request.setQueryParam(path.substring(0,path.indexOf("=")), path.substring( (path.indexOf("=")+1)));
-            path = "";
-        }
-
-        //HTTP version parse
-        request.setVersion(firstTextLine[2]);
-
-        //header parse
-        for (int i=1; i<HTTPmessages.length; i++ ){
-            if ( !HTTPmessages[i].contains(":")&& !HTTPmessages[i].contains("\n")){
-                request.setBody(HTTPmessages[i]);
-            }
-            request.setHeaderParam( HTTPmessages[i].substring(0,HTTPmessages[i].indexOf(":")) , HTTPmessages[i].substring(HTTPmessages[i].indexOf(":")+2) );
-        }
-
-    }
-
-}//end of class
+}
--- src/main/java/response/CustomHttpResponse.java
+++ src/main/java/response/CustomHttpResponse.java
@@ -1,25 +1,36 @@
 package response;
 
 import handler.GsonTool;
+
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 public class CustomHttpResponse {
-  public final Map<String,String> headers;
-  public final String status;
-  public final String version;
-  public final String body;
+    public final Map<String,String> headers;
+    public final String status;
+    public final String version;
+    public final String body;
 
-  public CustomHttpResponse(Map<String, String> headers, String status, String version,
-      String body) {
-    this.headers = headers;
-    this.status = status;
-    this.version = version;
-    this.body = body;
-  }
+    public CustomHttpResponse(Map<String, String> headers, String status, String version,
+                              String body) {
+        this.headers = headers;
+        this.status = status;
+        this.version = version;
+        this.body = body;
+    }
 
-  // TODO fill this out
-  public String toString(){
-    return null;
-  }
+    // TODO fill this out
+    public String toString(){
+        String output = version + " " + status + "\n";
+
+        Iterator<String> iterator = headers.keySet().iterator();
+        while (iterator.hasNext()){
+            String key = iterator.next();
+            output+= key +": " + headers.get(key) +"\n";
+        }
+        output+= "\n" + body;
+        return output;
+    }
 }
