bf29582949081f0e7b9021e3e5e2d27ac2393192
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2023-03-26 03:44:21 -0700

    more parsing issues.

--- src/main/java/request/CustomParser.java
+++ src/main/java/request/CustomParser.java
@@ -4,7 +4,7 @@ public class CustomParser {
 
     // extract java useable values from a raw http request string
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
-    public static ParsedRequest parse(String input){
+    public static ParsedRequest parse(String input) {
 
         ParsedRequest request = new ParsedRequest();
         String[] HTTPmessages = input.split("\n");
@@ -15,23 +15,22 @@ public class CustomParser {
 
         //path parse
         String path = firstTextLine[1];
-        if (!path.contains("?")){
+        if (!path.contains("?")) {
             request.setPath(path);
             path = "";
-        }
-        else{
-            request.setPath(path.substring(0,path.indexOf("?")));
-            path = path.substring(path.indexOf("?")+1);
+        } else {
+            request.setPath(path.substring(0, path.indexOf("?")));
+            path = path.substring(path.indexOf("?") + 1);
         }
 
         //query parse
-        while(path.length()!=0){
-            if (path.contains("&")){//more than 1 query parameter
-                request.setQueryParam(path.substring(0,path.indexOf("=")), path.substring( (path.indexOf("=")+1),path.indexOf("&")));
-                path = path.substring(path.indexOf("&")+1);
+        while (path.length() != 0) {
+            if (path.contains("&")) {//more than 1 query parameter
+                request.setQueryParam(path.substring(0, path.indexOf("=")), path.substring((path.indexOf("=") + 1), path.indexOf("&")));
+                path = path.substring(path.indexOf("&") + 1);
                 continue;
             }
-            request.setQueryParam(path.substring(0,path.indexOf("=")), path.substring( (path.indexOf("=")+1)));
+            request.setQueryParam(path.substring(0, path.indexOf("=")), path.substring((path.indexOf("=") + 1)));
             path = "";
         }
 
@@ -39,33 +38,32 @@ public class CustomParser {
         request.setVersion(firstTextLine[2]);
 
         //header parse
-//        if (HTTPmessages.length>=2){
-//            for (int i=1; i<HTTPmessages.length; i++ ){
-//                if (  (!(HTTPmessages[i].contains("\n"))) && (i== HTTPmessages.length)  )  {
-//                    request.setBody(HTTPmessages[i]);
-//                }
-//                else if (HTTPmessages[i].length()>2){
-//                    request.setHeaderParam( HTTPmessages[i].substring(0,HTTPmessages[i].indexOf(":")) , HTTPmessages[i].substring(HTTPmessages[i].indexOf(":")+2) );
-//                }
-//            }
-//        }
         boolean hasBody = false;
-        if (HTTPmessages.length>=2){
-            for (int i=1; i<HTTPmessages.length; i++ ){
-                System.out.println("current item for iteration: " + HTTPmessages[i]);
-                HTTPmessages[i] = HTTPmessages[i].replace(" ", "").replace("\n","").replace("\r","");
-                if ( HTTPmessages[i].contains("{") || (HTTPmessages[i].contains("}")) || (HTTPmessages.length<=2) ){
-                    System.out.println("we hit a curly brace or a newline char");
+        if (HTTPmessages.length >= 2) {
+            for (int i = 1; i < HTTPmessages.length; i++) {
+                System.out.println("current item for iteration: " + HTTPmessages[i] + "with length = " + HTTPmessages[i].length()+ " and the hasBody = " + hasBody );
+
+                HTTPmessages[i] = HTTPmessages[i].replace(" ", "");
+
+                if ( (i==HTTPmessages.length-2) || HTTPmessages[i].contains("{") || (HTTPmessages[i].contains("}"))  )  {
+                    System.out.println("we in the brace or a newline char area ");
                     hasBody = true;
-                    continue;
                 }
-                if(  (hasBody && (i==HTTPmessages.length-2))   ){
-                    String strWithoutWhiteSpace = HTTPmessages[i].substring(HTTPmessages[i].indexOf("\""));
-                    System.out.println("substring: " + strWithoutWhiteSpace);
-                    request.setBody("{" + strWithoutWhiteSpace + "}");
+
+                if ( hasBody&&(i==(HTTPmessages.length-2)) || hasBody&&(i==(HTTPmessages.length-1))    )  {
+                    if (HTTPmessages[i - 1].contains("{")) {
+//                        String strWithoutWhiteSpace = HTTPmessages[i].substring(HTTPmessages[i].indexOf("\""));
+                        String strWithoutWhiteSpace = HTTPmessages[i].replace(" ","");
+                        System.out.println("substring: " + strWithoutWhiteSpace);
+//                        request.setBody(HTTPmessages[i-1] + strWithoutWhiteSpace + HTTPmessages[i+1]);
+                        System.out.println("getBody() = " + request.getBody());
+                    } else {
+                        request.setBody(HTTPmessages[i]);
+                    }
                 }
-                else if (HTTPmessages[i].length()>3 && (!HTTPmessages[i].contains("{") || !HTTPmessages[i].contains("}") )){
-                    request.setHeaderParam( HTTPmessages[i].substring(0,HTTPmessages[i].indexOf(":")) , HTTPmessages[i].substring(HTTPmessages[i].indexOf(":")) );
+                else if (HTTPmessages[i].length()>4  ){
+//                    System.out.println("We're in the header area");
+                    request.setHeaderParam(HTTPmessages[i].substring(0, HTTPmessages[i].indexOf(":")), HTTPmessages[i].substring(HTTPmessages[i].indexOf(":")));
                 }
             }
         }
