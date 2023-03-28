8b69e28d201c63af734e71a11f9d86f55c428153
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2023-03-27 23:30:21 -0700

    Completed FallbackHandler and passed NotFoundTest.

--- src/main/java/handler/FallbackHandler.java
+++ src/main/java/handler/FallbackHandler.java
@@ -10,6 +10,6 @@ public class FallbackHandler implements BaseHandler {
     @Override
     public CustomHttpResponse handleRequest(ParsedRequest request) {
         var res =  new RestApiAppResponse<>(false, null, null);
-        return new ResponseBuilder().setStatus("404 Not Found").build();
+        return new ResponseBuilder().setStatus("HTTP/1.1 404 Not Found\n").build();
     }
 }
