3e53cd86f63b758a90f64e261cc622b750f27c16
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2023-03-29 17:48:06 -0700

    Cleaned up test print statesments. All tests passed.

--- src/main/java/handler/GetMessagesHandler.java
+++ src/main/java/handler/GetMessagesHandler.java
@@ -15,25 +15,6 @@ import response.RestApiAppResponse;
 
 public class GetMessagesHandler implements BaseHandler {
 
-//    @Override
-//    public CustomHttpResponse handleRequest(ParsedRequest request) {
-//
-//        String fromId = request.getQueryParam("fromId");
-//        String toId = request.getQueryParam("toId");
-//
-//        MessageDao messageDao = MessageDao.getInstance();
-//        List<MessageDto> listOfAllMessageDtos = messageDao.getAll();
-//        List<MessageDto> filteredListofMessageDtos = new ArrayList<>();
-//        for (MessageDto dto : listOfAllMessageDtos){
-//            if ((dto.getFromId()==fromId)&&(dto.getToId()==toId)){
-//                filteredListofMessageDtos.add(dto);
-//            }
-//        }
-//        var res = new RestApiAppResponse<>(true, filteredListofMessageDtos, null);
-//
-//        return new ResponseBuilder().setStatus("200 OK").setBody(GsonTool.gson.toJson(res)).build();
-//    }
-
     @Override
     public CustomHttpResponse handleRequest(ParsedRequest request) {
 
@@ -43,21 +24,40 @@ public class GetMessagesHandler implements BaseHandler {
         MessageDao messageDao = MessageDao.getInstance();
         List<MessageDto> listOfAllMessageDtos = messageDao.getAll();
         List<MessageDto> filteredListofMessageDtos = new ArrayList<>();
-        System.out.println("Here are the DTO data of size: " + listOfAllMessageDtos.size());
         for (MessageDto dto : listOfAllMessageDtos){
-            System.out.println("Current iteration: ");
-            System.out.println("fromId: " + dto.getFromId() +"\n" + "toId: " + dto.getToId());
-            System.out.println("\n");
             if ((dto.getFromId().equals(fromId))&&(dto.getToId().equals(toId))){
-                System.out.println("found a matching " + "\n" + "fromId: " + dto.getFromId() + "\n" + "toID: " + dto.getToId());
                 filteredListofMessageDtos.add(dto);
             }
         }
-        System.out.println("size of filteredListofMessageDtos: " + filteredListofMessageDtos.size());
-
         var res = new RestApiAppResponse<>(true, filteredListofMessageDtos, null);
-        System.out.println(GsonTool.gson.toJson(res));
+
         return new ResponseBuilder().setStatus("200 OK").setBody(GsonTool.gson.toJson(res)).build();
     }
+
+//    @Override
+//    public CustomHttpResponse handleRequest(ParsedRequest request) {
+//
+//        String fromId = request.getQueryParam("fromId");
+//        String toId = request.getQueryParam("toId");
+//
+//        MessageDao messageDao = MessageDao.getInstance();
+//        List<MessageDto> listOfAllMessageDtos = messageDao.getAll();
+//        List<MessageDto> filteredListofMessageDtos = new ArrayList<>();
+//        System.out.println("Here are the DTO data of size: " + listOfAllMessageDtos.size());
+//        for (MessageDto dto : listOfAllMessageDtos){
+//            System.out.println("Current iteration: ");
+//            System.out.println("fromId: " + dto.getFromId() +"\n" + "toId: " + dto.getToId());
+//            System.out.println("\n");
+//            if ((dto.getFromId().equals(fromId))&&(dto.getToId().equals(toId))){
+//                System.out.println("found a matching " + "\n" + "fromId: " + dto.getFromId() + "\n" + "toID: " + dto.getToId());
+//                filteredListofMessageDtos.add(dto);
+//            }
+//        }
+//        System.out.println("size of filteredListofMessageDtos: " + filteredListofMessageDtos.size());
+//
+//        var res = new RestApiAppResponse<>(true, filteredListofMessageDtos, null);
+//        System.out.println(GsonTool.gson.toJson(res));
+//        return new ResponseBuilder().setStatus("200 OK").setBody(GsonTool.gson.toJson(res)).build();
+//    }
 
 }
